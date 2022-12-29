2a179b7e1b9b8ca6809f570f48b256329cdf4760
Rosemberg Bezerra <rosemberg.bezerra@gmail.com>
2022-12-29 17:39:56 +0000

    solving bug when validate most common language

--- maven-archetype-visualnuts/src/main/java/pt/visualnuts/challenge2/service/CountryService.java
+++ maven-archetype-visualnuts/src/main/java/pt/visualnuts/challenge2/service/CountryService.java
@@ -1,12 +1,18 @@
 package pt.visualnuts.challenge2.service;
 
+import java.awt.RenderingHints.Key;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import pt.visualnuts.challenge2.model.Country;
@@ -39,8 +45,18 @@ public class CountryService {
 		for (Country country : countries) {
 			allLanguages.addAll(Arrays.asList(country.getLanguages()));
 		}
+		Map<String, Long> result = allLanguages.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		Map<String, Long> finalMap = new LinkedHashMap<>();
+
+		result.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+				.forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+
+		Long key = Collections.max(finalMap.entrySet(), Map.Entry.comparingByValue()).getValue();
+
 		Set<String> duplicateCompanies = allLanguages.stream()
-				.filter(language -> Collections.frequency(allLanguages, language) > 1).collect(Collectors.toSet());
+				.filter(language -> Collections.frequency(allLanguages, language) == key).collect(Collectors.toSet());
 		return duplicateCompanies.toArray(new String[0]);
 	}
 
